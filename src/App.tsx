912f259f5f660f81d41044d1c5c6f60047c27f2d
aKirpichenok <a.kirpichenok99@gmail.com>
2022-07-09 13:02:24 +0300

    React FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Card, { CardVariant } from "./components/Card"
+
 const App = () => {
   return (
     <div>
-      WORK!
+      <Card variant={CardVariant.outlined} width="200px" height="200px">
+        <button>button</button>
+        <div>asdasd</div>
+      </Card>
     </div>
   )
 }
